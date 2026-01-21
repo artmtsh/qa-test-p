f054a21ebf51de8b208b0afaffdef702abfe5569
artmtsh <58192947+artmtsh@users.noreply.github.com>
2026-01-21 23:24:21 +0300

    Add AuthPage

---
+++ demo/src/test/java/protei/test/qa/pages/AuthPage.java
@@ -0,0 +1,60 @@
+package protei.test.qa.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class AuthPage {
+  private final By FIELD_EMAIL = By.id("loginEmail");
+  private final By FIELD_PASSWORD = By.id("loginPassword");
+  private final By BUTTON_AUTH = By.id("authButton");
+
+  private final By ERROR_EMAIL_FORMAT = By.id("emailFormatError");
+  private final By ERROR_INVALID_EMAIL_PASSWORD = By.id("invalidEmailPassword");
+  private final WebDriverWait wait;
+
+  public AuthPage(WebDriver driver) {
+    this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+  }
+
+  public boolean isInvalidEmailFormatVisible(){
+    try {
+      return wait.until(
+              ExpectedConditions.visibilityOfElementLocated(ERROR_EMAIL_FORMAT)
+      ).isDisplayed();
+    } catch (TimeoutException e) {
+      return false;
+    }
+  }
+
+  public boolean isInvalidEmailPasswordVisible(){
+    try {
+      return wait.until(
+              ExpectedConditions.visibilityOfElementLocated(ERROR_INVALID_EMAIL_PASSWORD)
+      ).isDisplayed();
+    } catch (TimeoutException e) {
+      return false;
+    }
+  }
+
+  public void authenticate(String email, String password){
+    wait.until(ExpectedConditions.visibilityOfElementLocated(FIELD_EMAIL)).sendKeys(email);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(FIELD_PASSWORD)).sendKeys(password);;
+    wait.until(ExpectedConditions.elementToBeClickable(BUTTON_AUTH)).click();
+  }
+
+  public boolean isAuthPageOpened(){
+    try {
+      boolean fieldPasswordExist = wait.until(ExpectedConditions.visibilityOfElementLocated(FIELD_PASSWORD)).isDisplayed();
+      boolean fieldEmailExist = wait.until(ExpectedConditions.visibilityOfElementLocated(FIELD_EMAIL)).isDisplayed();
+      boolean buttonAuthExist = wait.until(ExpectedConditions.elementToBeClickable(BUTTON_AUTH)).isDisplayed();
+      return fieldPasswordExist && fieldEmailExist && buttonAuthExist;
+    } catch (TimeoutException e){
+      return false;
+    }
+  }
+}
