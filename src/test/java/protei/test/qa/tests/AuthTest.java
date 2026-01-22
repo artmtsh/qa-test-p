32ba9eb6e5b01a527de5c77c0ec7c249bae03c01
artmtsh <58192947+artmtsh@users.noreply.github.com>
2026-01-23 02:44:09 +0300

    rename tests

--- src/test/java/protei/test/qa/tests/AuthTest.java
+++ src/test/java/protei/test/qa/tests/AuthTest.java
@@ -1,6 +1,6 @@
 package protei.test.qa.tests;
+
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 import protei.test.qa.pages.AuthPage;
@@ -9,22 +9,24 @@ import protei.test.qa.pages.InputsPage;
 public class AuthTest extends BaseTest {
   @ParameterizedTest
   @CsvFileSource(resources = "/defaultLoginData.csv", numLinesToSkip = 1)
-  void validLoginOpensInputsPage(String email, String password) {
+  void shouldLoginCorrectly_LoginAndPasswordAreCorrect(String email, String password) {
     AuthPage authPage = new AuthPage(webDriver);
     authPage.authenticate(email, password);
     InputsPage inputsPage = new InputsPage(webDriver);
     Assertions.assertTrue(inputsPage.isInputsPageOpened());
   }
+
   @ParameterizedTest
   @CsvFileSource(resources = "/invalidEmailFormat.csv", numLinesToSkip = 1)
-  void invalidEmailShowsFormatError(String email) {
+  void shouldShowEmailFormatError_whenEmailIsInvalid(String email) {
     AuthPage authPage = new AuthPage(webDriver);
     authPage.authenticate(email, "");
     Assertions.assertTrue(authPage.isInvalidEmailFormatVisible());
   }
+
   @ParameterizedTest
   @CsvFileSource(resources = "/invalidEmailPassword.csv", numLinesToSkip = 1)
-  void invalidEmailPasswordShowsAuthError(String email, String password) {
+  void shouldShowAuthError_whenEmailPasswordIsInvalid(String email, String password) {
     AuthPage authPage = new AuthPage(webDriver);
     authPage.authenticate(email, password);
     Assertions.assertTrue(authPage.isInvalidEmailPasswordVisible());
